d0c2b4fa5d1dcc95139405ec9c63650698f50b7c
Becanar <116558378+Becanar@users.noreply.github.com>
2025-02-10 01:31:16 +0100

    mejora vuelta grupo

--- app/src/main/java/com/example/practicaprom/AniadirGrupo.kt
+++ app/src/main/java/com/example/practicaprom/AniadirGrupo.kt
@@ -11,24 +11,43 @@ class AniadirGrupo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aniadir_grupo)
-        val bd=BD(this)
-        val btnInsertar=findViewById<Button>(R.id.btnInsertarGrupo)
-        val txtGrupo=findViewById<EditText>(R.id.txtCodGrupo)
+
+        val bd = BD(this)
+        val btnInsertar = findViewById<Button>(R.id.btnInsertarGrupo)
+        val txtGrupo = findViewById<EditText>(R.id.txtCodGrupo)
+
         btnInsertar.setOnClickListener {
-            if(txtGrupo.text.toString().isNotEmpty()){
-                Thread{
-                    val insertSuccess: Boolean=bd.guardarGrupo(txtGrupo.text.toString().toInt())
+            if (txtGrupo.text.toString().isNotEmpty()) {
+                // Iniciar un hilo para manejar la inserción en la base de datos
+                Thread {
+                    val insertSuccess: Boolean = bd.guardarGrupo(txtGrupo.text.toString().toInt())
+
                     runOnUiThread {
-                        Toast.makeText(
-                            this,
-                            getString(R.string.el_grupo_ya_existe),
-                            Toast.LENGTH_SHORT
-                        ).show()
+                        if (insertSuccess) {
+                            // Si la inserción fue exitosa, mostrar un Toast y regresar
+                            Toast.makeText(
+                                this,
+                                getString(R.string.grupo_creado_correctamente),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            finish() // Regresar a la actividad anterior
+                        } else {
+                            // Si no fue exitoso, mostrar un mensaje indicando que ya existe
+                            Toast.makeText(
+                                this,
+                                getString(R.string.el_grupo_ya_existe),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }.start()
-            }else{
-                Toast.makeText(this,
-                    getString(R.string.debes_poner_un_codigo_de_grupo_para_poder_crearlo), Toast.LENGTH_SHORT).show()
+            } else {
+                // Si el campo está vacío, mostrar un mensaje de advertencia
+                Toast.makeText(
+                    this,
+                    getString(R.string.debes_poner_un_codigo_de_grupo_para_poder_crearlo),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
